eae33c8ac1d3711dfb0a682e099152d84c0c3b15
LupusEternus <wilk.tomek@gmail.com>
2024-05-16 18:46:19 +0200

    Project creation+Entity Class created

---
+++ src/main/java/com/wilk/todo/entity/ToDo.java
@@ -0,0 +1,23 @@
+package com.wilk.todo.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "todos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ToDo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String description;
+    private boolean completed;
+}

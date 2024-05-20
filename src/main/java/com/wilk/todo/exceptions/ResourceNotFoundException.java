25d0f43c3bb30e5885b3f51c48024a72031ddf05
LupusEternus <wilk.tomek@gmail.com>
2024-05-20 19:59:03 +0200

    Added security, csrf disable, added user and password and basic auth.

--- src/main/java/com/wilk/todo/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/wilk/todo/exceptions/ResourceNotFoundException.java
@@ -4,6 +4,7 @@ package com.wilk.todo.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
